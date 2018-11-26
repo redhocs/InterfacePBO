24cecd5dd57e1855eb7e8863adc373897b615456
RikaYanti40 <rikayanti4039@gmail.com>
2018-11-26 15:11:44 +0700

    Tugas Interface PBO Nama Kelompok: Agung Laksono P 294 Rika Yanti 307 Saprillah 312

---
+++ src/program/BangunDatarTest.java
@@ -0,0 +1,18 @@
+package program;
+
+public class BangunDatarTest {public static void main(String[] args) {
+
+        
+        BangunDatar PP = new PersegiPanjang(8,10);
+        PP.luas();
+        PP.keliling();
+        BangunDatar P = new Persegi(5);
+        P.luas();
+        P.keliling();
+        BangunDatar L = new lingkaran(7);
+        L.luas();
+        L.keliling();
+      
+ 
+    }
+}
